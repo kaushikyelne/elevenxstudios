8d345c17b64b6b2842b5bd04c097b2802e090122
Kaushik.Yelne <Kaushik.Yelne@dpworld.com>
2026-02-01 20:37:30 +0530

    feat(auth): define immutable user domain model and roles

--- modules/auth/domain/src/main/java/com/moneylane/modules/auth/domain/Role.java
+++ modules/auth/domain/src/main/java/com/moneylane/modules/auth/domain/Role.java
@@ -1,4 +1,6 @@
 package com.moneylane.modules.auth.domain;
 
 public enum Role {
+    USER,
+    ADMIN
 }
--- modules/auth/domain/src/main/java/com/moneylane/modules/auth/domain/User.java
+++ modules/auth/domain/src/main/java/com/moneylane/modules/auth/domain/User.java
@@ -1,4 +1,14 @@
 package com.moneylane.modules.auth.domain;
 
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class User {
+    private final UserId id;
+    private final String email;
+    private final String passwordHash;
+    private final Role role;
+    private final String status;
 }
