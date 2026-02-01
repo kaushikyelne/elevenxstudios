1360ccd7f619a9cd4f3db1f3685d38b39bba3347
Kaushik.Yelne <Kaushik.Yelne@dpworld.com>
2026-02-01 20:38:12 +0530

    build: anchor out/ in .gitignore to prevent ignoring package names

--- modules/auth/application/src/main/java/com/moneylane/modules/auth/application/port/in/RegisterUserUseCase.java
+++ modules/auth/application/src/main/java/com/moneylane/modules/auth/application/port/in/RegisterUserUseCase.java
@@ -1,4 +1,10 @@
 package com.moneylane.modules.auth.application.port.in;
 
+import com.moneylane.modules.auth.domain.User;
+
 public interface RegisterUserUseCase {
+    User register(Command command);
+
+    record Command(String email, String password) {
+    }
 }
